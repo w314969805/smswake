e18ac2b82b7fa5f161c1e5b3202c3e8ee73bbb1c
FMA-王卫星 <weixing.wang@futuremove.cn>
2018-01-25 16:14:52 +0800

    Initial commit

---
+++ src/main/java/com/fm/smswake/SmswakeApplication.java
@@ -0,0 +1,12 @@
+package com.fm.smswake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmswakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmswakeApplication.class, args);
+	}
+}
